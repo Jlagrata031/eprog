a7dbddfedd56841c74a946d7a692f5106d0db565
Jose Patricio R. Lagrata <jlagrata_031@yahoo.com>
2016-12-20 07:47:19 +0800

    Input and output grades

---
+++ input pogi ed.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+		int grade ;
+		
+		printf("Enter grade: ");
+		scanf ("%d", &grade);
+		printf("grade is : %d", grade);
+		if (grade%3%5 ==0){
+			printf("ed");
+		}else{
+			printf ("pogi");
+			}
+}
