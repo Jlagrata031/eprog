35eaf79ae34f82ca5309cab89df027c4dfa63ae2
Jose Patricio R. Lagrata <jlagrata_031@yahoo.com>
2017-01-10 09:35:24 +0800

    Create mmr.c

---
+++ mmr.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a, b, c;
+printf("Enter MMR of player 1: ");
+scanf("%d", &a);
+printf("Enter MMR of player 2: ");
+scanf("%d", &b);
+printf("%d", c= abs(a-b));
+
+
+
+}
